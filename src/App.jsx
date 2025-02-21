9958d5460728a857a9e9a747e48a9dc265254dfb
Akshay Chaturvedi <chaturvediakshay667@gmail.com>
2025-02-21 13:02:22 +0530

    typo in style in this I fix it

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,50 @@ import { useEffect, useState } from "react";
 
 function App() {
   return (
-    <div style={{backgroundColor:"#dfe6e9", height:"100vh"}}>
-     <div style={{display:"flex", justifyContent:"center"}}>
-      <div>
+    <div style={{ backgroundColor: "#dfe6e9", height: "100vh" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <div>
-         <PostComponent/>
-         <br />
+          <div>
+            <PostComponent />
+          </div>
+          <div>
+            <PostComponent />
+          </div>
         </div>
-        <div>
-         <PostComponent/>
-         <br />
-        </div>
-        
-         <PostComponent/>
-         
-      
       </div>
-     </div>
-
     </div>
   );
 }
 
 const style = {
-  width:200, backgroundColor:"white", borderRadius:10, borderColor:"gray", borderWidth:1, padding:20,
-}
+  width: 250,
+  backgroundColor: "white",
+  borderRadius: 10,
+  borderColor: "gray",
+  borderWidth: 1,
+  padding: 20,
+  margin: 10,
+};
 
-function PostComponent(){
-  return(
-    <div style={{style}}>
-      <div style={{display:"flex"}}>
-        <img src="https://cdn.pixabay.com/photo/2022/06/27/14/38/cat-7287671_1280.jpg" style={{width:30, height:30, borderRadius:20}} />
-        <div style={{fontSize:10, marginLeft:10}}>
+function PostComponent() {
+  return (
+    <div style={style}>
+      <div style={{ display: "flex" }}>
+        <img
+          src="https://cdn.pixabay.com/photo/2022/06/27/14/38/cat-7287671_1280.jpg"
+          style={{ width: 30, height: 30, borderRadius: 20 }}
+        />
+        <div style={{ fontSize: 10, marginLeft: 10 }}>
           <b>100xdevs</b>
           <div>23,098 followers</div>
           <div>12m</div>
         </div>
       </div>
-      <div style={{fontSize:12}}>
-        What to know how to win big? 
-        Check how folks win $6000 in bounties.
+      <div style={{ fontSize: 12 }}>
+        What to know how to win big? Check how folks win $6000 in bounties.
       </div>
     </div>
-  )
-  
+  );
 }
 
 export default App;
-
